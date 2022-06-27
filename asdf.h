b372adb31970c93f2d1ee75e598ab42ef0d4f54d
Sungwoo Kim <git@sung-woo.kim>
2022-06-27 10:18:02 -0400

    add const

---
+++ asdf.h
@@ -0,0 +1,23 @@
+#ifndef ASDF_H
+#define ASDF_H
+
+#define CONST1 1
+#define CONST2 2
+#define CONST3 3
+
+enum {
+  ENUM1 = 1,
+  ENUM4 = 4,
+  ENUM5,
+  ENUM6
+};
+
+
+struct mystruct {
+  int intfield;
+  char charfield;
+};
+
+
+
+#endif /* ASDF_H */
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include "asdf.h"
 
 int main () {
   printf("llvm-pass example\n");
+  printf("const1: %d\n", CONST1);
+  printf("enum4: %d\n", ENUM4);
 
   return 0;
 }
